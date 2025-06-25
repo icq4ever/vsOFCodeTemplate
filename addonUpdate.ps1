37dcf2dd8320f09501770d4ae002530cbec6578a
Yi donghoon <icq4ever@gmail.com>
2025-06-26 00:46:03 +0900

    addonUpdate tested and updated.

--- addonUpdate.ps1
+++ addonUpdate.ps1
@@ -1,46 +1,50 @@
 $ErrorActionPreference = "Stop"
 
-# 1. project locations
+# Project information
 $projectDir = Get-Location
 $projectName = Split-Path $projectDir -Leaf
 $vcxprojPath = "$projectDir\$projectName.vcxproj"
 $filtersPath = "$projectDir\$projectName.vcxproj.filters"
 $addonFile = "$projectDir\addon.make"
 $addonDest = "$projectDir\addons"
+$cppPropsPath = "$projectDir\.vscode\c_cpp_properties.json"
 
-# ✅ 2. openFrameworks relative root location
-$oFRoot = Resolve-Path "$projectDir\..\..\.."  
+# Find openFrameworks root relative to this project
+$oFRoot = Resolve-Path "$projectDir\..\..\.."
 
-# 3. copy addons by addon.make
+# Step 1: Copy addons
 if (Test-Path $addonFile) {
     $addons = Get-Content $addonFile | Where-Object { $_ -and -not $_.StartsWith("#") }
     foreach ($addon in $addons) {
         $src = Join-Path "$oFRoot\addons" $addon
         $dst = Join-Path $addonDest (Split-Path $addon -Leaf)
-        if (-Not (Test-Path $dst)) {
-            Write-Output "📦 Copying addon: $addon"
+        if (-not (Test-Path $dst)) {
+            Write-Host "📦 Copying addon: $addon"
             Copy-Item -Recurse -Force $src $dst
         }
     }
 }
 
-# 4. update .vcxproj 
+# Step 2: Update .vcxproj with ClInclude / ClCompile
 [xml]$proj = Get-Content $vcxprojPath
-$ns = @{ "msb" = "http://schemas.microsoft.com/developer/msbuild/2003" }
 $projRoot = $proj.Project
+$nsMgr = New-Object System.Xml.XmlNamespaceManager($proj.NameTable)
+$nsMgr.AddNamespace("ns", $proj.DocumentElement.NamespaceURI)
 
-function Add-ItemGroupEntry($itemType, $relativePath) {
-    $itemGroup = $proj.CreateElement("ItemGroup", $projRoot.NamespaceURI)
-    $fileNode = $proj.CreateElement($itemType, $projRoot.NamespaceURI)
-    $fileNode.SetAttribute("Include", $relativePath)
-    $itemGroup.AppendChild($fileNode) | Out-Null
+function Add-ItemGroupEntry {
+    param ($itemType, $relativePath)
+    $itemGroup = $proj.CreateElement("ItemGroup", $proj.DocumentElement.NamespaceURI)
+    $node = $proj.CreateElement($itemType, $proj.DocumentElement.NamespaceURI)
+    $node.SetAttribute("Include", $relativePath)
+    $itemGroup.AppendChild($node) | Out-Null
     $projRoot.AppendChild($itemGroup) | Out-Null
 }
 
-$existingIncludes = $proj.SelectNodes("//msb:ClInclude", $ns) | ForEach-Object { $_.Include }
-$existingCompiles = $proj.SelectNodes("//msb:ClCompile", $ns) | ForEach-Object { $_.Include }
+$existingIncludes = $projRoot.SelectNodes("//ns:ClInclude", $nsMgr) | ForEach-Object { $_.Include }
+$existingCompiles = $projRoot.SelectNodes("//ns:ClCompile", $nsMgr) | ForEach-Object { $_.Include }
 
 $allFiles = Get-ChildItem -Recurse -Include *.h,*.hpp,*.cpp,*.c -Path "$projectDir\src", "$projectDir\addons"
+
 foreach ($file in $allFiles) {
     $relPath = $file.FullName.Replace("$projectDir\", "").Replace("\", "/")
     if ($file.Extension -match "\.h|\.hpp") {
@@ -55,16 +59,11 @@ foreach ($file in $allFiles) {
 }
 
 $proj.Save($vcxprojPath)
-Write-Output "✅ Updated $vcxprojPath"
+Write-Host "✅ Updated $vcxprojPath"
 
-# 5. generate .filters 
-$filtersXml = @"
-<Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
-  <ItemGroup>
-"@
-
+# Step 3: Generate .filters
 function Make-Filter($path) {
-    $parts = $path -split '[\\/]', 0, 'SimpleMatch'
+    $parts = $path -split "[\\/]"
     if ($parts.Length -gt 1) {
         return ($parts[0..($parts.Length - 2)] -join '\')
     } else {
@@ -72,30 +71,61 @@ function Make-Filter($path) {
     }
 }
 
+$filtersXml = @()
+$filtersXml += '<?xml version="1.0" encoding="utf-8"?>'
+$filtersXml += '<Project ToolsVersion="4.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">'
+$filtersXml += '  <ItemGroup>'
+
 $filterMap = @{}
-$files = Get-ChildItem -Recurse -Include *.h,*.hpp,*.cpp,*.c -Path "$projectDir\src", "$projectDir\addons"
-
-foreach ($file in $files) {
+foreach ($file in $allFiles) {
     $relPath = $file.FullName.Replace("$projectDir\", "").Replace("\", "/")
-    $filter = Make-Filter($relPath)
+    $filter = Make-Filter $relPath
     $type = if ($file.Extension -match "\.h|\.hpp") { "ClInclude" } else { "ClCompile" }
-    $escapedPath = $relPath -replace '&', '&amp;' -replace '<', '&lt;' -replace '>', '&gt;'
-
-    $filtersXml += "    <$type Include=`"$escapedPath`">`n"
+    $filtersXml += "    <$type Include='" + $relPath + "'>"
     if ($filter -ne "") {
-        $filtersXml += "      <Filter>$filter</Filter>`n"
+        $filtersXml += "      <Filter>" + $filter + "</Filter>"
         $filterMap[$filter] = $true
     }
-    $filtersXml += "    </$type>`n"
+    $filtersXml += "    </$type>"
 }
 
-$filtersXml += "  </ItemGroup>`n<ItemGroup>`n"
+$filtersXml += '  </ItemGroup>'
+$filtersXml += '  <ItemGroup>'
+
 foreach ($f in $filterMap.Keys) {
-    $filtersXml += "    <Filter Include=`"$f`">`n"
-    $filtersXml += "      <UniqueIdentifier>{$(New-Guid).ToString()}</UniqueIdentifier>`n"
-    $filtersXml += "    </Filter>`n"
+    $guid = [guid]::NewGuid().ToString()
+    $filtersXml += "    <Filter Include='" + $f + "'>"
+    $filtersXml += "      <UniqueIdentifier>{" + $guid + "}</UniqueIdentifier>"
+    $filtersXml += "    </Filter>"
 }
-$filtersXml += "  </ItemGroup>`n</Project>"
 
-Set-Content -Path $filtersPath -Value $filtersXml -Encoding UTF8
-Write-Output "✅ Generated $filtersPath"
+$filtersXml += '  </ItemGroup>'
+$filtersXml += '</Project>'
+
+$filtersXml -join "`r`n" | Set-Content -Encoding UTF8 $filtersPath
+Write-Host "✅ Generated $filtersPath"
+
+# Step 4: Update c_cpp_properties.json for IntelliSense
+$includePaths = @(
+    "$oFRoot/libs/openFrameworks/**",
+    "$projectDir/src",
+    "$projectDir/addons/**"
+)
+
+$cppJson = @{
+    configurations = @(
+        @{
+            name = "Win64"
+            includePath = $includePaths
+            defines = @()
+            compilerPath = "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/*/bin/Hostx64/x64/cl.exe"
+            cStandard = "c17"
+            cppStandard = "c++17"
+            intelliSenseMode = "windows-msvc-x64"
+        }
+    )
+    version = 4
+}
+
+$cppJson | ConvertTo-Json -Depth 5 | Set-Content $cppPropsPath -Encoding UTF8
+Write-Host "✅ Updated $cppPropsPath"
